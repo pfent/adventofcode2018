88117d8b3925659a069196044504bc3bf02e8989
pfent <philipp@fent.de>
2018-12-02 19:43:51 +0100

    day1p2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,15 +1,39 @@
 use std::io;
 use std::io::prelude::*;
+use std::collections::HashSet;
 
 fn main() {
     let input = io::stdin();
-    let sum: i32 = input.lock()
-        .lines()
-        .map(&|line: Result<String, std::io::Error>| {
-            line.unwrap()
-                .parse::<i32>()
-                .unwrap()
-        }).sum();
+    let lines = input.lock().lines();
+    let numbers: Vec<i32> = lines.map(&|line: Result<String, std::io::Error>| {
+        line.unwrap()
+            .parse::<i32>()
+            .unwrap()
+    }).collect();
 
+    let sum = sum_values(&numbers);
     println!("{}", sum);
+
+    let duplicate: i32 = find_first_duplicate_sum(&numbers);
+    println!("{}", duplicate);
+}
+
+/// For part one
+fn sum_values(numbers: &Vec<i32>) -> i32 {
+    numbers.iter().sum()
+}
+
+/// For part two
+fn find_first_duplicate_sum(numbers: &Vec<i32>) -> i32 {
+    let mut seen_numbers = HashSet::<i32>::new();
+    let mut current: i32 = 0;
+    loop {
+        for number in numbers {
+            if seen_numbers.contains(&current) {
+                return current;
+            }
+            seen_numbers.insert(current);
+            current += number;
+        }
+    }
 }
