2f0fccd76bd82adebb12d078a8b7a81365dd276f
pfent <philipp@fent.de>
2018-12-16 15:45:07 +0100

    day03 WIP

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -3,8 +3,10 @@ extern crate regex;
 use std::io;
 use std::io::prelude::*;
 use regex::Regex;
+use std::collections::HashSet;
 
 struct Area {
+    claim_no: usize,
     x: usize,
     y: usize,
     x_len: usize,
@@ -12,7 +14,7 @@ struct Area {
 }
 
 fn main() {
-    let regex = Regex::new(r"#\d* @ (\d*),(\d*): (\d*)x(\d*)").unwrap();
+    let regex = Regex::new(r"#(\d)* @ (\d*),(\d*): (\d*)x(\d*)").unwrap();
 
     let input = io::stdin();
     let input: Vec<Area> = input.lock()
@@ -21,10 +23,11 @@ fn main() {
         .map(|line| {
             let capture = regex.captures(&line).unwrap();
             Area {
-                x: capture.get(1).unwrap().as_str().parse().unwrap(),
-                y: capture.get(2).unwrap().as_str().parse().unwrap(),
-                x_len: capture.get(3).unwrap().as_str().parse().unwrap(),
-                y_len: capture.get(4).unwrap().as_str().parse().unwrap(),
+                claim_no: capture.get(1).unwrap().as_str().parse().unwrap(),
+                x: capture.get(2).unwrap().as_str().parse().unwrap(),
+                y: capture.get(3).unwrap().as_str().parse().unwrap(),
+                x_len: capture.get(4).unwrap().as_str().parse().unwrap(),
+                y_len: capture.get(5).unwrap().as_str().parse().unwrap(),
             }
         }).collect();
 
@@ -40,6 +43,13 @@ fn main() {
 
     let overlapping = count_overlapping(&fabric);
     println!("overlapping squares: {}", overlapping);
+
+    let claims: HashSet<usize> = input.iter()
+        .map(|area: &Area| { area.claim_no })
+        .collect();
+
+
+    eliminate_overlapping(&fabric, claims);
 }
 
 fn count_overlapping(fabric: &[[i32; 1000]; 1000]) -> usize {
@@ -47,6 +57,15 @@ fn count_overlapping(fabric: &[[i32; 1000]; 1000]) -> usize {
         .flat_map(|row| row.iter())
         .filter(|i| **i > 1)
         .count()
+}
+
+fn eliminate_overlapping(fabric: &[[i32; 1000]; 1000], mut claims: HashSet<usize>) -> HashSet<usize> {
+    fabric.iter()
+        .flat_map(|row| row.iter())
+        .filter(|i| **i > 1);
+        //.for_each(|i| claims.remove(i.into()))
+
+    claims
 }
 
 
